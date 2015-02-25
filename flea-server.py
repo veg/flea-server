01cf59c23d448ec9912b757b0fd8ef65db84f018
Kemal Eren <kemal@kemaleren.com>
2015-02-24 22:30:39 -0800

    landing page

--- flea-server.py
+++ flea-server.py
@@ -1,7 +1,21 @@
-from bottle import route, run
+import glob
+from os import path
+
+from bottle import route, run, template
+
+
+DATA_DIR = "/home/kemal/projects/env/veg-hiv-env"
 
 @route('/')
-def hello():
-    return "Hello World!"
+def index():
+    files = glob.glob(path.join(DATA_DIR, '*/sequences.json'))
+    names = list(path.split(path.dirname(f))[1]
+                 for f in files)
+    links = list('<li><a href="/{name}">{name}</a></li>'.format(name=n) for n in names)
+    header = 'FLEA test server. Available data:\n<ul>\n'
+    middle = '\n'.join(links)
+    footer = '</ul>'
+    html = '\n'.join([header, middle, footer])
+    return template(html)
 
 run(host='localhost', port=8090, debug=True)
