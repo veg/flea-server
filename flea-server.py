002b415363c544341a8566afc4de05cd20af82eb
Kemal Eren <kemal@kemaleren.com>
2015-02-25 00:37:20 -0800

    implemented api and added mock data

--- flea-server.py
+++ flea-server.py
@@ -1,21 +1,86 @@
 import glob
+import os
 from os import path
+import json
 
 from bottle import route, run, template
+from bottle import static_file
+from bottle import response
 
 
-DATA_DIR = "/home/kemal/projects/env/veg-hiv-env"
+SERVER_DIR = path.dirname(path.realpath(__file__))
+FRONTEND_DIR = path.join(SERVER_DIR, 'flea-ember-app')
+DATA_DIR = path.join(SERVER_DIR, 'mock-data')
+
+NAME_MAPPER = {
+    'frequencies': 'frequencies.json',
+    'structure': 'secondaryStruct.pdb',
+    'trees': 'trees.json',
+    'neutralization': 'mab.json',
+    'sequences': 'sequences.json',
+    'rates_pheno': 'rates_pheno.json',
+    'dates': 'dates.json',
+    'rates': 'rates.json',
+}
+
+def parent(f):
+    return path.split(path.dirname(f))[1]
+
+
+def sessions():
+    files = glob.glob(path.join(DATA_DIR, '*/sequences.json'))
+    names = list(parent(f) for f in files)
+    return names
+
 
 @route('/')
 def index():
-    files = glob.glob(path.join(DATA_DIR, '*/sequences.json'))
-    names = list(path.split(path.dirname(f))[1]
-                 for f in files)
-    links = list('<li><a href="/{name}">{name}</a></li>'.format(name=n) for n in names)
+    links = list('<li><a href="/session/{name}/">{name}</a></li>'.format(name=n)
+                 for n in sessions())
     header = 'FLEA test server. Available data:\n<ul>\n'
     middle = '\n'.join(links)
     footer = '</ul>'
     html = '\n'.join([header, middle, footer])
     return template(html)
+
+
+@route('/assets/<filename>')
+def serve_static(filename):
+    root = os.path.join(FRONTEND_DIR, 'assets')
+    return static_file(filename, root=root)
+
+
+@route('/fonts/<filename>')
+def server_font(filename):
+    root = os.path.join(FRONTEND_DIR, 'fonts')
+    try:
+        idx = filename.index('?')
+        filename = filename[:idx]
+    except:
+        pass
+    print(filename)
+    return static_file(filename, root=root)
+
+
+@route('/api/<session_id>/<resource>')
+def data_file(session_id, resource):
+    if session_id not in sessions():
+        return {}
+    filename = path.join(DATA_DIR, session_id, NAME_MAPPER[resource])
+    with open(filename) as handle:
+        contents = handle.read()
+    if filename[-len('json'):] == 'json':
+        response.content_type = 'application/json'
+    return contents
+
+
+@route('/session/<session>/')
+def serve_session(session):
+    if session not in sessions():
+        return "{} not found".format(session)
+    result = path.join(FRONTEND_DIR, 'index.html')
+    with open(result) as handle:
+        return template(handle.read())
+
 
 run(host='localhost', port=8090, debug=True)
